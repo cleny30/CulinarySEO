4e3e0f6cc645ff3962682c51ab0ea39d4d17d5dd
dangngo18 <danghaingo.work@gmail.com>
2025-08-12 12:41:07 +0700

    feat: integrate redux-persist for state management and add product slice

--- CulinaryFE/src/redux/store.ts
+++ CulinaryFE/src/redux/store.ts
@@ -1,11 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import productReducer from "./product/productSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  blacklist: ["product"], // Product will not be persisted
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  product: productReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 

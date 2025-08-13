e78a791361658a0bdf388d4c2691944b9763fd2a
dangngo18 <danghaingo.work@gmail.com>
2025-08-13 15:06:57 +0700

    feat: enhance authentication components with back button and improved styles

--- CulinaryFE/src/components/auth/card-wrapper.tsx
+++ CulinaryFE/src/components/auth/card-wrapper.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import Social from "./social";
+import BackButton from "./back-button";
 
 interface CardWrapperProps {
   children: React.ReactNode;
-  headerLabel?: string;
   showSocial?: boolean;
+  backButtonHref?: string;
+  backButtonLabel?: string;
 }
 
 export default function CardWrapper({
   children,
-  headerLabel,
   showSocial,
+  backButtonHref = "/",
+  backButtonLabel = "",
 }: CardWrapperProps) {
   return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>{headerLabel}</CardHeader>
+    <Card className="w-[400px] shadow-md gap-y-4">
+      <CardHeader>{showSocial && <Social />}</CardHeader>
       <CardContent>{children}</CardContent>
-      <CardFooter>{showSocial && <Social />}</CardFooter>
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
     </Card>
   );
 }
--- CulinaryFE/src/utils/assets/icon.tsx
+++ CulinaryFE/src/utils/assets/icon.tsx
@@ -1,10 +1,11 @@
-import { Mail, Lock, Eye, EyeClosed } from "lucide-react";
+import { Mail, Lock, Eye, EyeClosed, type LucideProps } from "lucide-react";
 import { GoogleIcon } from "@/assets/svg/google";
+import type { SVGProps } from "react";
 
 export const Icon = {
-  Email: <Mail />,
-  Lock: <Lock />,
-  Eye: <Eye />,
-  EyeClosed: <EyeClosed />,
-  Google: <GoogleIcon />,
+  Email: (props: LucideProps) => <Mail {...props} />,
+  Lock: (props: LucideProps) => <Lock {...props} />,
+  Eye: (props: LucideProps) => <Eye {...props} />,
+  EyeClosed: (props: LucideProps) => <EyeClosed {...props} />,
+  Google: (props: SVGProps<SVGSVGElement>) => <GoogleIcon {...props} />,
 };

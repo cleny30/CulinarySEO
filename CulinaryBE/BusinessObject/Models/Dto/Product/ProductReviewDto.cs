fb1a89453c61c2f4c0c181bf0e4966c24e20128e
Nam Tran <namtmh31@gmail.com>
2025-08-13 10:40:49 +0700

    feat: enhance product service to calculate final price and average rating for products

--- CulinaryBE/BusinessObject/Models/Dto/Product/ProductReviewDto.cs
+++ CulinaryBE/BusinessObject/Models/Dto/Product/ProductReviewDto.cs
@@ -8,9 +8,10 @@ namespace BusinessObject.Models.Dto
     public class ProductReviewDto
     {
         public Guid ReviewId { get; set; }
-        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
         public int? Rating { get; set; }
         public string Comment { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     }
 }

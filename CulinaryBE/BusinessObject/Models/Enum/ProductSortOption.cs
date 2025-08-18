aad3d409a756b7f2ab47c777a17631edaedbec49
cleny <kushirotm@gmail.com>
2025-08-18 14:22:11 +0700

    feat: add ElasticSearch for get filter product

---
+++ CulinaryBE/BusinessObject/Models/Enum/ProductSortOption.cs
@@ -0,0 +1,13 @@
+﻿namespace BusinessObject.Models.Enum
+{
+    public enum ProductSortOption
+    {
+        PriceLowHigh,
+        PriceHighLow,
+        NameAZ,
+        NameZA,
+        BestSelling,
+        DateNewOld,
+        DateOldNew
+    }
+}

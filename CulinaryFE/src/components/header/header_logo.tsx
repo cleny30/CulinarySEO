c0bb06a3d6e518759b6a9dc65f4bd613db7cfa09
dangngo18 <danghaingo.work@gmail.com>
2025-08-28 16:45:11 +0700

    Refactor HeaderLogo component: update styling and remove unused code

--- CulinaryFE/src/components/header/header_logo.tsx
+++ CulinaryFE/src/components/header/header_logo.tsx
@@ -3,29 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function HeaderLogo() {
   return (
-    <Link to={"/"} className={``}>
-      <Logo color="#222222" className="not-lg:h-12"/>
+    <Link to={"/"} className={`ml-2`}>
+      <Logo color="#222222" className="not-lg:h-10"/>
     </Link>
   );
 }
 
-// import { useDispatch } from "react-redux";
-// import styles from "@/assets/css/home.module.css";
-// import { Button } from "../button";
-// import { useNavigate } from "react-router-dom";
-// import { logout } from "@/redux/auth/apiRequest";
-// import { printToast } from "@/utils/constants/toast/printToast";
-
-// const dispatch = useDispatch();
-// const navigate = useNavigate();
-// const handleLogout = async () => {
-//   await logout(dispatch, navigate).then(printToast);
-// };
-{
-  /* Hello {user?.fullName}
-{user && (
-  <Button variant={"secondary"} type="button" onClick={handleLogout}>
-    Logout
-  </Button>
-)} */
-}

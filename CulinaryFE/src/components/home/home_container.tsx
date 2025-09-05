9d6dda431d7f8a90dedc25d71030fa734a51524b
dangngo18 <danghaingo.work@gmail.com>
2025-09-05 16:05:41 +0700

    Implement code changes to enhance functionality and improve performance

--- CulinaryFE/src/components/home/home_container.tsx
+++ CulinaryFE/src/components/home/home_container.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HeroSection from "./home_hero_section";
-import HomeSection2 from "./home_session_2";
+import IntroduceSection from "./home_session_2";
+import { storeInfo } from "@/storeInfo";
 
 export default function HomeContainer() {
   return (
     <>
       <HeroSection />
-      <HomeSection2 />
+      {storeInfo.section_introduce_show && <IntroduceSection />}
     </>
   );
 }
